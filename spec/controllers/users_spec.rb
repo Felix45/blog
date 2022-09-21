0b6bdac6639722426f99288a7a927e5fadeb185f
Felix Ouma <fatonoh@gmail.com>
2022-09-21 21:41:26 +0300

    Add unit test for testing response status & template rendered

---
+++ spec/controllers/users_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  before :each do
+    @user = User.new(
+      name: 'Felix',
+      bio: 'Software Developer',
+      photo: 'http://hello.com/org.png',
+      posts_counter: 0
+    )
+    @user.save
+  end
+
+  it 'renders the index template' do
+    get '/users/'
+    expect(response.status).to eq(200)
+    expect(response).to render_template('index')
+  end
+
+  it 'renders the show template' do
+    get '/users/1'
+    expect(response.status).to eq(200)
+    expect(response).to render_template('show')
+  end
+end
