2cd6c32f5eeca038be26eb23fb532080e8572b8e
Felix Ouma <fatonoh@gmail.com>
2022-09-21 22:07:53 +0300

    Add tests for rendered template and response status

--- spec/controllers/posts_spec.rb
+++ spec/controllers/posts_spec.rb
@@ -1,6 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', :type => ['request', 'feature'] do
+  before :each do
+    @user = User.new(
+      name: 'Felix',
+      bio: 'Software Developer',
+      photo: 'http://hello.com/org.png',
+      posts_counter: 0
+    )
+    @user.save
+  end
+
+  it 'renders the correct posts page' do
+    get '/users/1/posts'
+    expect(response.status).to eq(200)
+    expect(response).to render_template('index')
+  end
+
+  it 'renders the correct single post page' do
+    get '/users/1/posts/1'
+    expect(response.status).to eq(200)
+    expect(response).to render_template('show')
+  end
+
   it 'renders index template with right text' do
     visit '/users/1/posts/'
     expect(page).to have_text('Users posts')
