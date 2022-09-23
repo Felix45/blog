ef6dca1f8e875f05ce87edbe9a37714c723e6c0c
Felix Ouma <fatonoh@gmail.com>
2022-09-24 02:26:03 +0300

    Add form for user to create a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,13 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id].to_i)
   end
 
+  def new
+    newpost = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { newpost: } }
+    end
+  end
+
   def show
     @post = Post.find(params[:id])
   end
