85be812de01c976bfa2aa7d6e296d5a4c646739e
Felix Ouma <fatonoh@gmail.com>
2022-09-24 03:24:02 +0300

    Add form to create a new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,29 @@ class PostsController < ApplicationController
     end
   end
 
+  def create
+    @post = current_user.posts.new(post_params)
+    @post.likes_counter = 0
+    @post.comments_counter = 0
+
+    respond_to do |format|
+      format.html do
+        if @post.save
+          @post.update_posts_counter
+          redirect_to user_post_path(current_user, @post.id)
+        else
+          newpost = Post.new
+          render :new , locals: { newpost: }
+        end
+      end
+    end
+  end
+
   def show
     @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
