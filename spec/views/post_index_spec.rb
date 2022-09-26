969511be8a1e8e212819c1fdbf902275297d31ff
Felix Ouma <fatonoh@gmail.com>
2022-09-27 02:15:23 +0300

    Fix linter errors using rubocop

--- spec/views/post_index_spec.rb
+++ spec/views/post_index_spec.rb
@@ -3,53 +3,16 @@ require 'rails_helper'
 RSpec.describe 'Post', type: :system do
   describe 'index page' do
     before(:each) do
-      @user =
-        User.create(
-          name: 'Tom',
-          bio: 'Software Developer',
-          photo:
-            'http://hello.com/org.png',
-          posts_counter: 4
-        )
-      @post =
-        Post.create(
-          title: 'one post',
-          text: 'first post',
-          author_id: @user.id,
-          comments_counter: 0,
-          likes_counter: 0
-        )
-      @post_one =
-        Post.create(
-          title: 'two post',
-          text: 'second post',
-          author_id: @user.id,
-          comments_counter: 0,
-          likes_counter: 0
-        )
-      @post_two =
-        Post.create(
-          title: 'three post',
-          text: 'third post',
-          author_id: @user.id,
-          comments_counter: 0,
-          likes_counter: 0
-        )
-      Comment.create(
-        text: 'welcome to the city',
-        author_id: @user.id,
-        post_id: @post_one.id
-      )
-      Comment.create(
-        text: 'This movie is good',
-        author_id: @user.id,
-        post_id: @post_one.id
-      )
-      Comment.create(
-        text: 'I love Africa',
-        author_id: @user.id,
-        post_id: @post_two.id
-      )
+      @user = User.create(name: 'Tom', bio: 'Software Developer', photo: 'http://hello.com/org.png', posts_counter: 0)
+      @post = Post.create(title: 'one post', text: 'first post', author_id: @user.id, comments_counter: 0,
+                          likes_counter: 0)
+      @post_one = Post.create(title: 'two post', text: 'second post', author_id: @user.id, comments_counter: 0,
+                              likes_counter: 0)
+      @post_two = Post.create(title: 'three post', text: 'third post', author_id: @user.id, comments_counter: 0,
+                              likes_counter: 0)
+      Comment.create(text: 'welcome to the city', author_id: @user.id, post_id: @post_one.id)
+      Comment.create(text: 'This movie is good', author_id: @user.id, post_id: @post_one.id)
+      Comment.create(text: 'I love Africa', author_id: @user.id, post_id: @post_two.id)
       Like.create(post_id: @post.id, author_id: @user.id)
       Like.create(post_id: @post_one.id, author_id: @user.id)
       Like.create(post_id: @post_two.id, author_id: @user.id)
