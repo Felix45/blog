1cc42df13af530328c18556bb24afadae3d7d127
Felix Ouma <fatonoh@gmail.com>
2022-09-27 01:47:51 +0300

    Fix linter errors using rubocop

--- spec/views/post_index_spec.rb
+++ spec/views/post_index_spec.rb
@@ -1,52 +1,52 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "Post", type: :system do
-  describe "index page" do
+RSpec.describe 'Post', type: :system do
+  describe 'index page' do
     before(:each) do
       @user =
         User.create(
-          name: "Tom",
-          bio: "Software Developer",
+          name: 'Tom',
+          bio: 'Software Developer',
           photo:
-            "http://hello.com/org.png",
+            'http://hello.com/org.png',
           posts_counter: 4
         )
       @post =
         Post.create(
-          title: "one post",
-          text: "first post",
+          title: 'one post',
+          text: 'first post',
           author_id: @user.id,
           comments_counter: 0,
           likes_counter: 0
         )
       @post_one =
         Post.create(
-          title: "two post",
-          text: "second post",
+          title: 'two post',
+          text: 'second post',
           author_id: @user.id,
           comments_counter: 0,
           likes_counter: 0
         )
       @post_two =
         Post.create(
-          title: "three post",
-          text: "third post",
+          title: 'three post',
+          text: 'third post',
           author_id: @user.id,
           comments_counter: 0,
           likes_counter: 0
         )
       Comment.create(
-        text: "welcome to the city",
+        text: 'welcome to the city',
         author_id: @user.id,
         post_id: @post_one.id
       )
       Comment.create(
-        text: "This movie is good",
+        text: 'This movie is good',
         author_id: @user.id,
         post_id: @post_one.id
       )
       Comment.create(
-        text: "I love Africa",
+        text: 'I love Africa',
         author_id: @user.id,
         post_id: @post_two.id
       )
@@ -56,7 +56,7 @@ RSpec.describe "Post", type: :system do
     end
     it "I can see the user's username." do
       visit user_posts_path(@user.id)
-      expect(page).to have_content("Tom")
+      expect(page).to have_content('Tom')
     end
 
     it "I can see the user's profile picture." do
@@ -64,40 +64,40 @@ RSpec.describe "Post", type: :system do
       expect(page).to have_xpath("//img[contains(@src,'http://hello.com/org.png')]")
     end
 
-    it "I can see the number of posts each user has written." do
+    it 'I can see the number of posts each user has written.' do
       visit user_posts_path(@user.id)
       expect(page).to have_content(3)
     end
 
     it "I can see the user's username." do
       visit user_posts_path(@user.id)
-      expect(page).to have_content("one post")
-      expect(page).to have_content("two post")
-      expect(page).to have_content("three post")
+      expect(page).to have_content('one post')
+      expect(page).to have_content('two post')
+      expect(page).to have_content('three post')
     end
 
     it "I can see some of the post's body." do
       visit user_posts_path(@user.id)
-      expect(page).to have_content("first post")
-      expect(page).to have_content("second post")
-      expect(page).to have_content("third post")
+      expect(page).to have_content('first post')
+      expect(page).to have_content('second post')
+      expect(page).to have_content('third post')
     end
 
-    it " can see the first comments on a post." do
+    it ' can see the first comments on a post.' do
       visit user_posts_path(@user.id)
-      expect(page).to have_content("welcome to the city")
-      expect(page).to have_content("This movie is good")
+      expect(page).to have_content('welcome to the city')
+      expect(page).to have_content('This movie is good')
     end
 
-    it "I can see how many comments a post has." do
+    it 'I can see how many comments a post has.' do
       visit user_posts_path(@user.id)
-      expect(page).to have_content("Comments: 2")
-      expect(page).to have_content("Comments: 1")
+      expect(page).to have_content('Comments: 2')
+      expect(page).to have_content('Comments: 1')
     end
 
-    it "I can see how many likes a post has." do
+    it 'I can see how many likes a post has.' do
       visit user_posts_path(@user.id)
-      expect(page).to have_content("Likes: 1")
+      expect(page).to have_content('Likes: 1')
     end
 
     it 'redirects to all user posts page' do
@@ -105,6 +105,5 @@ RSpec.describe "Post", type: :system do
       click_link 'one post'
       expect(page).to have_current_path user_post_path(@user.id, @post.id)
     end
-
   end
 end
