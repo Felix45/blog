057abd869f608e84117080cc778cb68b4e8506de
Felix Ouma <fatonoh@gmail.com>
2022-09-29 00:55:36 +0300

    Add devise routes to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users 
+
   devise_scope  :user do
     get 'users/sign_out' => 'devise/sessions#destroy'
   end
