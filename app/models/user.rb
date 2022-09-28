cdadafa832804fa93a8ed300f364d6c903c90308
Felix Ouma <fatonoh@gmail.com>
2022-09-29 01:05:02 +0300

    Add default values upon user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,19 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
-  before_save :default_values
+  before_validation :default_values
 
   validates :name, presence: true
-  #validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_posts
     posts.order('created_at Desc').last(3)
   end
 
+  private
+  
   def default_values
+    self.posts_counter = 0;
     self.photo = 'https://img.myloview.com/posters/social-media-user-icon-default-avatar-profile-image-400-251200036.jpg' if self.photo.nil?
   end
 end
