0539c99037cf77633a5f5ee3d2fcc29ce0f81b3e
ridwan <ridwanediallo99@gmail.com>
2022-09-29 17:44:50 +0000

    Add authorization

---
+++ app/models/ability.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    if user.role == 'admin'
+      can :manage, :all
+    else
+      can :destroy, Post do |post|
+        post.author == user
+      end
+
+      can :destroy, Comment do |comment|
+        comment.author == user
+      end
+
+      can :create, Post
+      can :create, Comment
+    end
+  end
+end
