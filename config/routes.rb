4089eee6203e57532f5c5e6224a6c667d8997673
Felix Ouma <fatonoh@gmail.com>
2022-10-01 11:30:26 +0300

    Add API routes to the application

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  post '/auth/login', to: 'authentication#login'
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :create, :show] do
+        resources :posts do
+          resources :comments, only: [:index, :create]
+        end
+      end
+    end
+  end
+
   devise_for :users 
 
   devise_scope  :user do
